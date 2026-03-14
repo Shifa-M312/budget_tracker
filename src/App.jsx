708bfc5fad62fb19155ec177df0c0974c5e8934d
Shifa-M312 <shifanarasheedm@gmail.com>
2026-03-15 03:07:09 +0530

    Day 1: Vite + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-green-600">
+        Tailwind Test Successful 
+      </h1>
+    </div>
+  )
+}
+
+export default App
